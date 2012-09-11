007ff20c535cd983d1ee53569b2218469cea3aed
TIHan <lol.tihan@gmail.com>
2012-09-10 21:43:39 -0700

    added vc10 solution file

--- datagame-protocol/src/test.cpp
+++ datagame-protocol/src/test.cpp
@@ -1,11 +1,44 @@
+#ifdef __GNUC__
+  typedef int8_t dgp_int8;
+  typedef uint8_t dgp_uint8;
+  typedef int16_t dgp_int16;
+  typedef uint16_t dgp_uint16;
+  typedef int32_t dgp_int32;
+  typedef uint32_t dgp_uint32;
+  typedef int64_t dgp_int64;
+  typedef uint64_t dgp_uint64;
+#elif _MSC_VER
+  typedef signed __int8 dgp_int8;
+  typedef unsigned __int8 dgp_uint8;
+  typedef signed __int16 dgp_int16;
+  typedef unsigned __int16 dgp_uint16;
+  typedef signed __int32 dgp_int32;
+  typedef unsigned __int32 dgp_uint32;
+  typedef signed __int64 dgp_int64;
+  typedef unsigned __int64 dgp_uint64;
+#endif
+
+typedef struct _dgp_uint128
+{
+  dgp_uint16 value[8];
+} dgp_uint128;
+  
+typedef void* dgp_pointer;
+typedef float dgp_float;
+typedef double dgp_double;
+
+#ifdef __GNUC__
+  #include <sys/socket.h>
+  #include <netdb.h>
+  #include <arpa/inet.h>
+  #include <netinet/in.h>
+#elif _MSC_VER
+  #include <winsock2.h>
+  #include <ws2tcpip.h>
+
+  #pragma comment(lib, "ws2_32.lib")
+#endif
 #include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include "dgp_types.h"
 
 #define MAX_BUFFER_LENGTH 1024
 
@@ -15,6 +48,18 @@ static dgp_int32 g_iSocketFileDescriptorIpV6 = -1;
 dgp_int32 dgp_init (dgp_uint16 port) {
   struct addrinfo hints, *res, *p;
   char portbuf[sizeof port], ip[INET6_ADDRSTRLEN];
+
+#ifdef _MSC_VER
+  WSADATA wsaData;
+  int iResult;
+
+  // Initialize Winsock
+  iResult = WSAStartup (MAKEWORD(2,2), &wsaData);
+  if (iResult != 0) {
+    printf("WSAStartup failed: %d\n", iResult);
+    return 1;
+  }
+#endif
   
   memset (&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;          /* IPv4 and/or IPv6 */
@@ -43,15 +88,13 @@ dgp_int32 dgp_init (dgp_uint16 port) {
     return 1;
   }
   
-  
-  
-  
   return 0;
 }
 
 dgp_int32 main ()
 {
   dgp_init (46767);
-
+  //WSACleanup ();
+    system ("pause");
   return -1;
 }
