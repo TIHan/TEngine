81fd9ceeeac6d3c811051864e72f2fde24390052
TIHan <lol.tihan@gmail.com>
2012-09-12 02:27:48 -0700

    fixed windows

--- datagame-protocol/src/network.hpp
+++ datagame-protocol/src/network.hpp
@@ -9,14 +9,18 @@ namespace dgp {
 
   typedef struct sockaddr_in dgpAddress;
   typedef struct sockaddr_in6 dgpAddress6;
+  typedef struct addrinfo dgpAddressInfo;
 
   class network {
     dgpInt m_iFileDescriptorSocket;
     dgpInt m_iFileDescriptorSocket6;
     dgpAddress *m_pAddress;
     dgpAddress6 *m_pAddress6;
+    dgpAddressInfo *m_pAddressInfo;
 
     dgpByte *m_bStream;
+
+    dgpInt close (dgpInt sockfd);
 
   public:
     network (dgpUshort usPort);
