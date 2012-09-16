665e5d820defd2c5084d696b0c250bffdf65b212
TIHan <lol.tihan@gmail.com>
2012-09-16 18:28:41 -0500

    fixed some portability stuff

--- datagame-protocol/src/error.hpp
+++ datagame-protocol/src/error.hpp
@@ -3,12 +3,18 @@
 
 #include <stdio.h>
 
+#define MAX_ERROR_LEN 256
+
+#if _MSC_VER
+  #define snprintf _snprintf_s
+#endif
+
 #define ERROR_MESSAGE(str) \
   fprintf (stderr, "(%s) Error: %s at line %i.\n", __FUNCTION__, str, __LINE__); \
 
 #define ERROR_MESSAGE_FORMAT(str, ...) \
-  char newstr[255]; \
-  sprintf_s (newstr, str, __VA_ARGS__); \
+  char newstr[MAX_ERROR_LEN]; \
+  snprintf (newstr, MAX_ERROR_LEN, str, __VA_ARGS__); \
   ERROR_MESSAGE(newstr) \
 
 #endif // ERROR_HPP
