18106d37519d259967f86e7b815dddb52abb211b
tihan <tihan@ubuntu.(none)>
2012-09-28 00:38:45 -0700

    now compiles fine on linux :)

--- src/testing/test.cpp
+++ src/testing/test.cpp
@@ -5,7 +5,7 @@ using namespace TE;
 
 TEint main ()
 {
-   // Network *network = new Network ();
+    //Network *network = new Network ();
   for (int i = 0; i < 50000; i++) {
     Packet *packet = new Packet (new ByteStream (512), "192.168.1.1", "888");
     delete packet;
