0271173a1444ad596dc93b03d72dc1d8736bde13
TIHan <lol.tihan@gmail.com>
2012-09-28 00:55:02 -0500

    oops, forgot some stuff

---
+++ src/lib/te/ByteStream.cpp
@@ -0,0 +1,235 @@
+/*
+  Copyright (c) 2012, William F. Smith
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation 
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+  THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "ByteStream.hpp"
+
+namespace TE {
+  class PByteStream : public IByteStream {
+    TEbyte *m_pbStream;
+    TEbyte *m_pbReadPosition;
+    TEbyte *m_pbWritePosition;
+    TEuint m_nSize;
+    TEuint m_nMaxSize;
+    bool m_bError;
+
+  public:
+    explicit PByteStream(const TEuint nMaxSize);
+    ~PByteStream();
+
+    TEbyte* GetCopyOfStream ();
+    TEuint GetSize ();
+    TEuint GetMaxSize ();
+    void Clear ();
+    bool HasError ();
+
+    TEbyte ReadByte ();
+    void WriteByte (const TEbyte byte);
+  };
+
+  /*!
+   *
+   */
+  TEchar* IByteStream::ReadString () {
+    TEuint size = GetSize ();
+    TEchar *val = new TEchar[size];
+
+    for (TEuint i = 0; i < size; i++) {
+      if (HasError ()) {
+        delete [] val;
+        return 0;
+      }
+      val[i] = ReadByte ();        
+      if (val[i] == '\0') {
+        return val;
+      }
+    }
+    delete [] val;
+    return 0;
+  }
+
+  /*!
+   *
+   */
+  void IByteStream::WriteString (const TEchar *sz) {
+    TEuint size = (TEint)strlen (sz);
+
+    for (TEuint i = 0; i < size; i++) {
+      if (HasError ()) {
+        return;
+      }
+      WriteByte (sz[i]);
+      if (i + 1 >= size) {
+        WriteByte ('\0');
+      }
+    }
+  }
+
+  /*!
+   *
+   */
+  void IByteStream::WriteStream (const TEbyte *pbStream, const TEuint nSize) {
+    for (TEuint i = 0; i < nSize; i++) {
+      if (HasError ()) {
+        return;
+      }
+      WriteByte (pbStream[i]);
+    }
+  }
+
+  /*!
+   * \brief The constructor for the ByteStream that requires
+   * a maximum size for the ByteStream buffer.
+   *
+   */
+  PByteStream::PByteStream (const TEuint nMaxSize) {
+    m_nMaxSize = nMaxSize;
+    m_pbStream = new TEbyte[m_nMaxSize];
+    m_pbReadPosition = m_pbStream;
+    m_pbWritePosition = m_pbStream;
+    m_nSize = 0;
+    m_bError = false;
+  }
+
+  /*!
+   *
+   */
+  PByteStream::~PByteStream () {
+    WARNING_IF_FORMAT(m_nSize != 0, "Not all bytes were read. Please clear or read the remaining %d byte(s).", m_nSize)
+    delete [] m_pbStream;
+  }
+
+  /*!
+   *
+   */
+  TEbyte* PByteStream::GetCopyOfStream () {
+    TEbyte *stream = new TEbyte[m_nMaxSize];
+    memcpy (stream, m_pbStream, m_nMaxSize);
+    return stream;
+  }
+
+  /*!
+   *
+   */
+  TEuint PByteStream::GetSize () {
+    return m_nSize;
+  }
+
+  /*!
+   *
+   */
+  TEuint PByteStream::GetMaxSize () {
+    return m_nMaxSize;
+  }
+
+  /*!
+   *
+   */
+  void PByteStream::Clear () {
+    delete [] m_pbStream;
+    m_pbStream = new TEbyte[m_nMaxSize];
+    m_pbReadPosition = m_pbStream;
+    m_pbWritePosition = m_pbStream;
+    m_nSize = 0;
+    m_bError = false;
+  }
+
+  /*!
+   *
+   */
+   bool PByteStream::HasError () {
+    return m_bError;
+   }
+
+  /*!
+   *
+   */
+  TEbyte PByteStream::ReadByte () {
+    TEuint size = sizeof (TEbyte);
+
+    if (m_nSize - size > m_nMaxSize) {
+      m_bError = true;
+      return 0;
+    }
+
+    TEbyte val = *m_pbReadPosition;
+    m_pbReadPosition++;
+    m_nSize -= size;
+    return val;
+  }
+
+  /*!
+   *
+   */
+  void PByteStream::WriteByte (const TEbyte byte) {
+    TEuint size = sizeof (TEbyte);
+
+    if (m_nSize + size > m_nMaxSize) {
+      m_bError = true;
+      return;
+    }
+
+    *m_pbWritePosition = byte;
+    m_pbWritePosition++;
+    m_nSize += size;
+  }
+
+
+  ByteStream::ByteStream (const TEuint nMaxSize) : priv (new PByteStream (nMaxSize)) {
+  }
+
+  ByteStream::~ByteStream () {
+    delete priv;
+  }
+
+  TEbyte* ByteStream::GetCopyOfStream () {
+    return priv->GetCopyOfStream ();
+  }
+
+  TEuint ByteStream::GetSize () {
+    return priv->GetSize ();
+  }
+
+  TEuint ByteStream::GetMaxSize () {
+    return priv->GetMaxSize ();
+  }
+
+  void ByteStream::Clear () {
+    priv->Clear ();
+  }
+
+  bool ByteStream::HasError () {
+    return priv->HasError ();
+  }
+
+  TEbyte ByteStream::ReadByte () {
+    return priv->ReadByte ();
+  }
+
+  void ByteStream::WriteByte (const TEbyte byte) {
+    priv->WriteByte (byte);
+  }
+}
---
+++ src/lib/te/Types.hpp
@@ -0,0 +1,71 @@
+/*
+  Copyright (c) 2012, William F. Smith
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification,
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation 
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+  THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef __TYPES_HPP_
+#define __TYPES_HPP_
+
+#ifdef __GNUC__
+  #include <stdint.h>
+#endif
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+namespace TE {
+#ifdef __GNUC__
+  typedef char TEchar;
+  typedef int8_t TEint8;
+  typedef uint8_t TEbyte;
+  typedef int16_t TEshort;
+  typedef uint16_t TEushort;
+  typedef int32_t TEint;
+  typedef uint32_t TEuint;
+  typedef int64_t TEint64;
+  typedef uint64_t TEuint64;
+#elif _MSC_VER
+  typedef char TEchar;
+  typedef signed __int8 TEint8;
+  typedef unsigned __int8 TEbyte;
+  typedef signed __int16 TEshort;
+  typedef unsigned __int16 TEushort;
+  typedef signed __int32 TEint;
+  typedef unsigned __int32 TEuint;
+  typedef signed __int64 TEint64;
+  typedef unsigned __int64 TEuint64;
+#endif
+
+  typedef struct _TEuint128 {
+    TEushort value[8];
+  } TEuint128;
+  
+  typedef float TEfloat;
+  typedef double TEdouble;
+}
+
+#endif /* __TYPES_HPP_ */
