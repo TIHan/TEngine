d596e7e36d37af9f9b3112d55407e913867e7d2d
TIHan <lol.tihan@gmail.com>
2012-10-22 20:25:28 -0500

    changed testing

--- src/test.cpp
+++ src/test.cpp
@@ -5,12 +5,12 @@ using namespace TE;
 
 TEint main()
 {
-  TEuint asdf = System::GetTicks();
-  for (int i = 0; i < 5; i++) {
-    shared_ptr<Thread> thread(new Thread([=] { cout << "Thread " << i << "\n"; }));
-    //thread->Join();
-  }
+  //TEuint asdf = System::GetTicks();
   //cout << System::GetTicks() - asdf << endl;
+  unique_ptr<Thread> thread(new Thread([=] { cout << "Thread 1\n"; }));
+  unique_ptr<Thread> thread2(new Thread([=] { cout << "Thread 2\n"; }));
+  unique_ptr<Thread> thread3(new Thread([=] { cout << "Thread 3\n"; }));
+  unique_ptr<Thread> thread4(new Thread([=] { cout << "Thread 4\n"; }));
 #ifdef _MSC_VER
     system("pause");
 #endif
