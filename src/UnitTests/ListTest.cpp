dec80e2649f46e93509cf2a6c36883255f89de34
TIHan <lol.tihan@gmail.com>
2012-11-05 23:29:35 -0600

    Engine project now in a separate folder. Wrote more list tests.

--- src/UnitTests/ListTest.cpp
+++ src/UnitTests/ListTest.cpp
@@ -14,7 +14,43 @@ TEST_F(ListTest, IsEmptyInitially) {
   EXPECT_EQ(0, l1_.GetSize());
 }
 
-TEST_F(ListTest, AddingWorks) {
+TEST_F(ListTest, ModifyingWorks) {
   l1_.Add(5);
   EXPECT_EQ(1, l1_.GetSize());
+  l1_.Remove(5);
+  EXPECT_EQ(0, l1_.GetSize());
+  l1_.Add(6);
+  l1_.Add(6);
+  EXPECT_EQ(2, l1_.GetSize());
+  l1_.Remove(6);
+  EXPECT_EQ(1, l1_.GetSize());
+  l1_.Add(7);
+  EXPECT_EQ(2, l1_.GetSize());
+  l1_.Remove(8);
+  EXPECT_EQ(2, l1_.GetSize());
+  l1_.Remove(6);
+  l1_.Remove(7);
+  EXPECT_EQ(0, l1_.GetSize());
+}
+
+TEST_F(ListTest, QueryingWorks) {
+  l1_.Add(6);
+  l1_.Add(2);
+  l1_.Add(3);
+  l1_.Add(6);
+  l1_.Add(5);
+  l1_.Add(6);
+  l1_.Add(8);
+  l1_.Add(8);
+  l1_.Add(10);
+  l1_.Add(10);
+  l1_.Add(10);
+  l1_.Add(10);
+
+  EXPECT_EQ(1, l1_.Where([] (TEint i) { return i == 2; })->GetSize());
+  EXPECT_EQ(1, l1_.Where([] (TEint i) { return i == 3; })->GetSize());
+  EXPECT_EQ(2, l1_.Where([] (TEint i) { return i == 8; })->GetSize());
+  EXPECT_EQ(4, l1_.Where([] (TEint i) { return i == 10; })->GetSize());
+  EXPECT_EQ(3, l1_.Where([] (TEint i) { return i == 6; })->GetSize());
+  EXPECT_EQ(0, l1_.Where([] (TEint i) { return i == 7; })->GetSize());
 }
