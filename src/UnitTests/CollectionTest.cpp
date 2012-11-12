9e59c834d148cc56847a7f196ef1176d27d323a2
TIHan <lol.tihan@gmail.com>
2012-11-11 23:45:42 -0600

    Adding CollectionTest.

---
+++ src/UnitTests/CollectionTest.cpp
@@ -0,0 +1,40 @@
+#include <TELib.hpp>
+#include <Network.hpp>
+#include <gtest/gtest.h>
+
+using namespace TE;
+
+class CollectionTest : public ::testing::Test {
+protected:
+  shared_ptr<IList<TEint>> l1_;
+  shared_ptr<ISequence<TEint>> s1_;
+};
+
+TEST_F(CollectionTest, IsEmptyInitially) {
+  l1_ = make_shared<List<TEint>>();
+  s1_ = make_shared<Sequence<TEint>>();
+  EXPECT_EQ(0, l1_->GetSize());
+  EXPECT_EQ(0, s1_->GetSize());
+}
+
+TEST_F(CollectionTest, TestSpeed) {
+  l1_ = make_shared<List<TEint>>();
+  s1_ = make_shared<Sequence<TEint>>();
+
+  TEuint ticks = System::GetTicks();
+  for (TEint i = 0; i < 50000; i++) {
+    l1_->Add(i);
+  }
+  cout << "=============================\n";
+  cout << "List 1 Test\n";
+  cout << "Time: " << System::GetTicks() - ticks << endl;
+  cout << "=============================\n";
+  ticks = System::GetTicks();
+  for (TEint i = 0; i < 50000; i++) {
+    s1_->Add(i);
+  }
+  cout << "=============================\n";
+  cout << "Sequence 1 Test\n";
+  cout << "Time: " << System::GetTicks() - ticks << endl;
+  cout << "=============================\n";
+}
