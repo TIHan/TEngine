a58b9a0b85e6a9f64ec6455b0ccdd9c5abbbaef2
TIHan <lol.tihan@gmail.com>
2012-11-16 02:36:58 -0600

    Fixed type test for different os/compilers.

--- src/UnitTests/SystemTest.cpp
+++ src/UnitTests/SystemTest.cpp
@@ -2,6 +2,7 @@
 #include <Network.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <chrono>
 
 using namespace TE;
 
@@ -19,12 +20,15 @@ TEST_F(SystemTest, Types) {
   EXPECT_EQ(2, static_cast<int>(sizeof(unsigned short)));
   EXPECT_EQ(4, static_cast<int>(sizeof(int)));
   EXPECT_EQ(4, static_cast<int>(sizeof(unsigned int)));
-  EXPECT_EQ(4, static_cast<int>(sizeof(long)));
-  EXPECT_EQ(4, static_cast<int>(sizeof(unsigned long)));
+// long has specific sizes for each OS
+//  EXPECT_EQ(4, static_cast<int>(sizeof(long)));
+//  EXPECT_EQ(4, static_cast<int>(sizeof(unsigned long)));
   EXPECT_EQ(8, static_cast<int>(sizeof(long long)));
   EXPECT_EQ(8, static_cast<int>(sizeof(unsigned long long)));
 
   EXPECT_EQ(4, static_cast<int>(sizeof(float)));
   EXPECT_EQ(8, static_cast<int>(sizeof(double)));
   EXPECT_EQ(1, static_cast<int>(sizeof(bool)));
+// void* has specific sizes for each OS
+//  EXPECT_EQ(4, static_cast<int>(sizeof(void*)));
 }
