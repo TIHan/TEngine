c2a21e835c007a0f67dbe35bcc57f4ab06efbc75
TIHan <lol.tihan@gmail.com>
2012-11-16 02:23:38 -0600

    Adding type testing.

--- src/UnitTests/SystemTest.cpp
+++ src/UnitTests/SystemTest.cpp
@@ -1,6 +1,7 @@
 #include <TELib.hpp>
 #include <Network.hpp>
 #include <gtest/gtest.h>
+#include <iostream>
 
 using namespace TE;
 
@@ -11,4 +12,19 @@ TEST_F(SystemTest, TicksAndDelay) {
 }
 
 TEST_F(SystemTest, Types) {
+  EXPECT_EQ(1, sizeof(char));
+  EXPECT_EQ(1, sizeof(signed char));
+  EXPECT_EQ(1, sizeof(unsigned char));
+  EXPECT_EQ(2, sizeof(short));
+  EXPECT_EQ(2, sizeof(unsigned short));
+  EXPECT_EQ(4, sizeof(int));
+  EXPECT_EQ(4, sizeof(unsigned int));
+  EXPECT_EQ(4, sizeof(long));
+  EXPECT_EQ(4, sizeof(unsigned long));
+  EXPECT_EQ(8, sizeof(long long));
+  EXPECT_EQ(8, sizeof(unsigned long long));
+
+  EXPECT_EQ(4, sizeof(float));
+  EXPECT_EQ(8, sizeof(double));
+  EXPECT_EQ(1, sizeof(bool));
 }
