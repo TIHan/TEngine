a2a381f0370b273c6b11e748a092548d4f655c5d
TIHan <lol.tihan@gmail.com>
2012-11-30 13:54:55 -0600

    Adding RealTest.

--- src/UnitTests/network_test.cc
+++ src/UnitTests/network_test.cc
@@ -23,12 +23,12 @@ TEST_F(NetworkTest, ConnectTest) {
   client.Connect("127.0.0.1", "1337");
 
   for (int i = 0; i < 512; ++i) {
-  auto connectMessage = client.CreateMessage(
-      network::ReservedClientMessage::kConnect);
-  for (int i = 0; i < 511; ++i) {
-    connectMessage->Write<uint8_t>(255);
-  }
-  connectMessage->Send();
+    auto connectMessage = client.CreateMessage(
+        network::ReservedClientMessage::kConnect);
+    for (int i = 0; i < 511; ++i) {
+      connectMessage->Write<uint8_t>(255);
+    }
+    connectMessage->Send();
   }
 
   std::cout << "Press any key to connect...\n";
@@ -38,6 +38,60 @@ TEST_F(NetworkTest, ConnectTest) {
   std::cout << "Press any key to process messages...\n";
   std::cin.get();
   server.ProcessMessages();
+}
+
+TEST_F(NetworkTest, RealTest) {
+  int choice = 0;
+  std::cout << "Enter 1 to host, 2 to connect.\n";
+  std::cin >> choice;
+
+  if (choice != 1 && choice != 2) {
+    return;
+  }
+
+  if (choice == 1) {
+    network::Server server(1338);
+    server.Start();
+    server.RegisterMessageCallback(network::ReservedClientMessage::kConnect,
+        [] (std::shared_ptr<network::ReceiveMessage> message) {
+      static int count;
+      count++;
+      std::cout << "Message " << count << std::endl;
+      for (int i = 0; i < 511; ++i) {
+        uint8_t hello = message->Read<uint8_t>();
+      }
+    });
+    std::cout << "Press any key to process messages.\n";
+    std::cin.get();
+    std::cin.get();
+    server.ProcessMessages();
+    return;
+  }
+
+  std::string address;
+  std::string port;
+
+  std::cout << "Enter address: ";
+  std::cin >> address;
+
+  std::cout << "Enter port: ";
+  std::cin >> port;
+
+  network::Client client;
+  client.Connect(address, port);
+
+  for (int i = 0; i < 512; ++i) {
+    auto connectMessage = client.CreateMessage(
+        network::ReservedClientMessage::kConnect);
+    for (int i = 0; i < 511; ++i) {
+      connectMessage->Write<uint8_t>(255);
+    }
+    connectMessage->Send();
+  }
+
+  std::cout << "Press any key to connect...\n";
+  std::cin.get();
+  client.SendMessages();
 }
 
                                                                                
