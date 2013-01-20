bc2ecefa9338a1e3600ab47f1e687881947bb672
TIHan <lol.tihan@gmail.com>
2013-01-20 04:39:41 -0600

    Added unit tests to test opengl context 3.2

--- src/UnitTests/glfw_test.cc
+++ src/UnitTests/glfw_test.cc
@@ -5,6 +5,19 @@
 class GLFWTest : public ::testing::Test {
 };
 
-TEST_F(GLFWTest, GLFW1) {
-  EXPECT_TRUE(glfwInit());
+TEST_F(GLFWTest, Initialize) {
+    EXPECT_TRUE(glfwInit());
+    glfwTerminate();
+}
+
+TEST_F(GLFWTest, Context3_2) {
+    EXPECT_TRUE(glfwInit());
+    
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    EXPECT_TRUE(glfwOpenWindow(640, 480, 0, 0, 0, 0, 32, 0, GLFW_WINDOW));
+    
+    glfwTerminate();
 }
