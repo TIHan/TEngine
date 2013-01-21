6c1361eddd65c11427cd0355fa5b2f79afec9699
Will Smith <tihan@tihan-Macmini.(none)>
2013-01-20 19:32:50 -0600

    Fixed linux.

--- src/UnitTests/glfw_test.cc
+++ src/UnitTests/glfw_test.cc
@@ -13,9 +13,6 @@ TEST_F(GLFWTest, Initialize) {
 TEST_F(GLFWTest, Context3_2) {
     EXPECT_TRUE(glfwInit());
     
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 0);
-    
     EXPECT_TRUE(glfwOpenWindow(640, 480, 0, 0, 0, 0, 32, 0, GLFW_WINDOW));
     
     glfwTerminate();
