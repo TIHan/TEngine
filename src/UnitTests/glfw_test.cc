1e10eae30b8a01eb206eb846be0040be2d7539fc
TIHan <lol.tihan@gmail.com>
2013-02-10 18:55:20 -0600

    Fixing glfw_test.

--- src/UnitTests/glfw_test.cc
+++ src/UnitTests/glfw_test.cc
@@ -7,35 +7,24 @@
 class GLFWTest : public ::testing::Test {
 };
 
-TEST_F(GLFWTest, Initialize) {
-    EXPECT_TRUE(glfwInit());
-    glfwTerminate();
-}
-
 TEST_F(GLFWTest, OpenWindow) {
   EXPECT_TRUE(glfwInit()); 
 
   glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
-#ifdef _WIN32
-  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
-  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#else
-# ifdef __APPLE__
+
+#ifdef __APPLE__
   glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
   glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
   glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-# elif
+#else
   glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
-  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 0);
-# endif
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
 
   EXPECT_TRUE(glfwOpenWindow(640, 360, 0, 0, 0, 0, 32, 0, GLFW_WINDOW));
   EXPECT_TRUE(glewInit() == GLEW_OK);
-
-
     
   glfwTerminate();
 }
